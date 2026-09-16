14b0eb7133d348f68ba1e5d5ad200d2960801dd0
LweZ <144471422+LweZ01@users.noreply.github.com>
2026-09-16 17:43:32 -0400

    Integrate AOS for animations and handle window resize
    Added AOS for animations and implemented resize handling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./index.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import AnimatedBackground from "./components/Background";
@@ -35,7 +37,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
               <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
               <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
                 © 2026{" "}
-                <a href="www.eki.my.id" className="hover:underline">
+                <a href="[www.eki.my.id](https://www.eki.my.id)" className="hover:underline">
                   LweZ
                 </a>
                 . All Rights Reserved.
@@ -56,7 +58,7 @@ const ProjectPageLayout = () => (
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
           © 2026{" "}
-          <a href="www.eki.my.id" className="hover:underline">
+          <a href="[www.eki.my.id](https://www.eki.my.id)" className="hover:underline">
             LweZ
           </a>
           . All Rights Reserved.
@@ -68,6 +70,29 @@ const ProjectPageLayout = () => (
 
 function App() {
   const [showWelcome, setShowWelcome] = useState(true);
+
+  useEffect(() => {
+    const initAOS = () => {
+      const isMobile = window.innerWidth < 768;
+      AOS.init({
+        once: isMobile ? true : false,
+      });
+    };
+
+    initAOS();
+
+    let resizeTimer;
+    const handleResize = () => {
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(initAOS, 250);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      clearTimeout(resizeTimer);
+    };
+  }, []);
 
   return (
     <BrowserRouter>
